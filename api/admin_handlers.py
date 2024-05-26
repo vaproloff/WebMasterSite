6a7258b37d6761133632a346292a516da0ac3c8b
chiiriill <chiiriill@yandex.ru>
2024-05-26 17:40:00 +0300

    Ref filterin by date in query table

--- api/admin_handlers.py
+++ api/admin_handlers.py
@@ -380,7 +380,8 @@ async def get_urls(request: Request, data_request: dict):
             else:
                 color = "#B4D7ED"
                 color_text = "black"
-            res[stat[0].strftime(date_format_2)] = f"""<div style='height: 55px; width: 100px; margin: 0px; padding: 0px; background-color: {color}'>
+            res[stat[0].strftime(
+                date_format_2)] = f"""<div style='height: 55px; width: 100px; margin: 0px; padding: 0px; background-color: {color}'>
             <span style='font-size: 18px'>{stat[1]}</span><span style="margin-left: 5px; font-size: 10px; color: {color_text}">{abs(up)}</span><br>
             <span style='font-size: 10px'>Клики</span><span style='font-size: 10px; margin-left: 20px'>CTR {stat[4]}%</span><br>
             <span style='font-size: 10px'>{stat[2]}</span> <span style='font-size: 10px; margin-left: 20px'>R {stat[3]}%</span>
@@ -399,37 +400,41 @@ async def get_urls(request: Request):
 
 
 @admin_router.post("/get-queries")
-async def get_urls(request: Request, length: int = Form(), start: int = Form(), start_date: datetime = Form(),
-                   end_date: datetime = Form(), amount: int = Form(default=14), search_text: str = Form(default=""), \
-                   sort_result: bool = Form(default=False), sort_desc: bool = Form(default=False)):
-    limit = length
-    offset = start + 1
-    if sort_result:
-        if search_text == "":
-            urls = await _get_urls_with_pagination_sort_query(offset, limit, start_date, end_date, sort_desc,
+async def get_urls(request: Request, data_request: dict):
+    start_date = datetime.strptime(data_request["start_date"], date_format_2)
+    end_date = datetime.strptime(data_request["end_date"], date_format_2)
+    if data_request["sort_result"]:
+        if data_request["search_text"] == "":
+            urls = await _get_urls_with_pagination_sort_query(data_request["start"], data_request["length"], start_date,
+                                                              end_date, data_request["sort_desc"],
                                                               async_session)
         else:
-            urls = await _get_urls_with_pagination_and_like_sort_query(offset, limit, start_date, end_date, search_text,
-                                                                       sort_desc,
+            urls = await _get_urls_with_pagination_and_like_sort_query(data_request["start"], data_request["length"],
+                                                                       start_date, end_date,
+                                                                       data_request["search_text"],
+                                                                       data_request["sort_desc"],
                                                                        async_session)
     else:
-        if search_text == "":
-            urls = await _get_urls_with_pagination_query(offset, limit, start_date, end_date, async_session)
+        if data_request["search_text"] == "":
+            urls = await _get_urls_with_pagination_query(data_request["start"], data_request["length"], start_date,
+                                                         end_date, async_session)
         else:
-            urls = await _get_urls_with_pagination_and_like_query(offset, limit, start_date, end_date, search_text,
+            urls = await _get_urls_with_pagination_and_like_query(data_request["start"], data_request["length"],
+                                                                  start_date, end_date, data_request["search_text"],
                                                                   async_session)
     try:
         grouped_data = [(key, sorted(list(group)[:14], key=lambda x: x[0])) for key, group in
                         groupby(urls, key=lambda x: x[-1])]
     except TypeError as e:
-        print(urls)
-        return
+        return JSONResponse({"data": []})
+
     if len(grouped_data) == 0:
-        return {"data": []}
+        return JSONResponse({"data": []})
+
     data = []
     for el in grouped_data:
-        res = [
-            f"<div style='width:355px; height: 55px; overflow: auto; white-space: nowrap;'><span>{el[0]}</span></div>"]
+        res = {"query":
+                   f"<div style='width:355px; height: 55px; overflow: auto; white-space: nowrap;'><span>{el[0]}</span></div>"}
         for k, stat in enumerate(el[1]):
             up = 0
             if k + 1 < len(el[1]):
@@ -443,21 +448,18 @@ async def get_urls(request: Request, length: int = Form(), start: int = Form(), 
             else:
                 color = "#B4D7ED"
                 color_text = "black"
-            res.append(f"""<div style='height: 55px; width: 100px; margin: 0px; padding: 0px; background-color: {color}'>
-            <span style='font-size: 18px'>{stat[1]}</span><span style="margin-left: 5px; font-size: 10px; color: {color_text}">{abs(up)}</span><br>
-            <span style='font-size: 10px'>Клики</span><span style='font-size: 10px; margin-left: 20px'>CTR {stat[4]}%</span><br>
-            <span style='font-size: 10px'>{stat[2]}</span> <span style='font-size: 10px; margin-left: 35px'>R {stat[3]}%</span>
-            </div>""")
-        res = pad_list_with_zeros(res, amount + 1)
-        test = res[::-1]
-        test.insert(0,
-                    f"<div style='width:355px; height: 55px; overflow: auto; white-space: nowrap;'><span>{el[0]}</span></div>")
-        data.append(test)
+            res[stat[0].strftime(
+                date_format_2)] = f"""<div style='height: 55px; width: 100px; margin: 0px; padding: 0px; background-color: {color}'>
+              <span style='font-size: 18px'>{stat[1]}</span><span style="margin-left: 5px; font-size: 10px; color: {color_text}">{abs(up)}</span><br>
+              <span style='font-size: 10px'>Клики</span><span style='font-size: 10px; margin-left: 20px'>CTR {stat[4]}%</span><br>
+              <span style='font-size: 10px'>{stat[2]}</span> <span style='font-size: 10px; margin-left: 20px'>R {stat[3]}%</span>
+              </div>"""
+        data.append(res)
 
     json_data = jsonable_encoder(data)
 
     # return JSONResponse({"data": json_data, "recordsTotal": limit, "recordsFiltered": 50000})
-    return JSONResponse({"data": json_data, "recordsTotal": limit})
+    return JSONResponse({"data": json_data})
 
 
 @admin_router.get("/info-queries")
