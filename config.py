1ff4e2b6b06549a50118c407b410803a2ba2039a
kiryazb <94808775+kiryazb@users.noreply.github.com>
2024-08-19 14:05:24 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-#config.py
-
-DB_USER = "dn_true"
-DB_PASSWORD = "1238xcnq&qaQWER"
-DB_HOST = "localhost"
-DB_PORT = "5432"
-SECRET = "c87d095c0d020fba6d5671484664c1825b9fe9c69ef59f324f95b96a2fa7ae09"
-
-DATABASE_GENERAL_NAME = "general"
